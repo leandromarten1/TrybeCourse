a560a89cc1e746c6d4ff3181241f0ff0e122e375
Leandro Marten <leandromarten1@gmail.com>
2020-03-26 19:15:37 -0300

    Upload exercise 8

--- exercises/4_1/exercise_8.js
+++ exercises/4_1/exercise_8.js
@@ -5,8 +5,7 @@ let num3 = 9;
 let isEven = false;
 
 if (num1 % 2 == 0 || num2 % 2 == 0 || num3 % 2 == 0){
-     isEven = true;
-     console.log(isEven);
-}else{
-    console.log(isEven);
+        isEven = true;
+     
 }
+console.log(isEven);
---
+++ exercises/4_1/exercise_9.js
@@ -0,0 +1,11 @@
+
+let num1 = 3;
+let num2 = 1;
+let num3 = 9;
+let isOdd = false;
+
+if (num1 % 2 === 1 || num2 % 2 === 1 || num3 % 2 === 1){
+        isOdd = true;
+     
+}
+console.log(isOdd);
