7740a95a3289ab1e94998a677d62dc7aa8264725
Leandro Marten <leandromarten1@gmail.com>
2020-03-26 17:23:34 -0300

    Upload exercise 4

---
+++ exercises/4_1/exercise_4.js
@@ -0,0 +1,13 @@
+// Make a program that, given a value defined in a variable, returns “positive” if that value is positive,
+// “negative” if it is negative and “zero” otherwise.
+
+
+let x = 0;
+
+if (x > 0){
+    console.log("Positive!");
+}else if (x < 0){
+    console.log("Negative!");
+}else{
+    console.log("Zero!")
+}
