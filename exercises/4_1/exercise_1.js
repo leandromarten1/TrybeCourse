a1ac51b6056006ba28cab37e2d0f80fdc147355d
Leandro Marten <leandromarten1@gmail.com>
2020-03-26 16:40:12 -0300

    change var to let

--- exercises/4_1/exercise_1.js
+++ exercises/4_1/exercise_1.js
@@ -2,30 +2,30 @@
 // Your program must have two variables, a and b, defined at the beginning with the values ​​to be operated.
 // Addition, subtraction, multiplication, division, module
 
-var a = 3;
-var b = 7;
+let a = 3;
+let b = 7;
 
 // Program 1 - Addition
 
-var x = a + b;
+let x = a + b;
 console.log(x);
 
 // Program 2 - Subtraction
 
-var x = a - b;
+let x = a - b;
 console.log(x);
 
 // Program 3 - Multiplication
 
-var x = a * b;
+let x = a * b;
 console.log(x);
 
 // Program 4 - Divison
 
-var x = a / b;
+let x = a / b;
 console.log(x);
 
 // Program 5 - Module
 
-var x = a % b;
+let x = a % b;
 console.log(x);
