c26404eb02e1ce35e632366eaec496cbebb6b07a
Leandro Marten <leandromarten1@gmail.com>
2020-03-30 17:43:53 -0300

    Upload exercise 2

---
+++ exercises/4_3/exercise_2.js
@@ -0,0 +1,11 @@
+//Exercise 2
+
+let info = {
+  personagem: "Margarida",
+  origem: "Pato Donald",
+  nota: "Namorada do personagem principal nos quadrinhos do Pato Donald",
+  //insert new key-value
+  recorrent: 'Sim'
+};
+
+console.log(info);
