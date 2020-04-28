a7b63c602cb652d2940c1815ff1cc56ed4554bdb
Leandro Marten <leandromarten1@gmail.com>
2020-04-28 18:33:55 -0300

    Iniciando JS

--- exercises/7_3/script.js
+++ exercises/7_3/script.js
@@ -1,8 +1,16 @@
 
+function getRandomCard() {
+  const cards = ['cinco-ouros', 'nove-espadas', 'quatro-paus', 'seis-copas', 'sete-espadas', 'sete-paus', 'tres-ouros'];
+    let randomCard = Math.floor(Math.random() * cards.length);
+    return cards[randomCard]
+}
+
 function addCard() {
   let element = document.createElement('div');
   let divMae = document.querySelector('.row');
+  let randomCard = getRandomCard();  
   element.classList = 'carta';
+  element.classList.add(randomCard);
   divMae.appendChild(element);
 
 }
