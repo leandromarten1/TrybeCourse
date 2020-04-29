6dce303691fbdd3621c40cb32c2ed8a8d3dea20a
Leandro Marten <leandromarten1@gmail.com>
2020-04-28 21:55:43 -0300

    Requisitos OK

--- exercises/7_3/script.js
+++ exercises/7_3/script.js
@@ -5,14 +5,21 @@ function getRandomCard() {
     return cards[randomCard]
 }
 
+function getRandomAnimation() {
+  const animations = ['rotate', 'flipx', 'flipy', 'scale'];
+  let randomAnimation = Math.floor(Math.random() * animations.length);
+  return animations[randomAnimation];
+}
+
 function addCard() {
   let element = document.createElement('div');
   let divMae = document.querySelector('.row');
-  let randomCard = getRandomCard();  
+  let randomCard = getRandomCard();
+  let randomAnim = getRandomAnimation(); 
   element.classList = 'carta';
   element.classList.add(randomCard);
+  element.classList.add(randomAnim);
   divMae.appendChild(element);
-
 }
 
 const baralho = document.querySelector('#baralho');
