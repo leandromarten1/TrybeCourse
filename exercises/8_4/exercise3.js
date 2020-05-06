6ec224d26c7c79710d8b7bd2916ee181c81bd28a
Leandro Marten <leandromarten1@gmail.com>
2020-05-06 19:01:55 -0300

    Ex3 R1

---
+++ exercises/8_4/exercise3.js
@@ -0,0 +1,12 @@
+const assert = require('assert');
+// escreva a função addAllnumbers para passar nos testes abaixo:
+
+const addAllNumbers = (arr) => {}
+
+//
+const numbers = [9, 23, 10, 3, 8];
+const expected = 53;
+const output = addAllnumbers(numbers);
+
+assert.strictEqual(typeof addAllnumbers, 'function');
+assert.strictEqual(output, expected);
