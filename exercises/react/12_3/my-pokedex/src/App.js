773890290c2baf610299ca11dd2a55994e2ca88d
Leandro Marten <leandromarten1@gmail.com>
2020-06-02 19:30:51 -0300

    Exercicio 1

---
+++ exercises/react/12_3/my-pokedex/src/App.js
@@ -0,0 +1,44 @@
+import React from 'react';
+import './App.css';
+import pokemons from './data';
+import Pokedex from './components/Pokedex';
+import Button from './components/Button';
+
+let totalPokes = 1;
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      pokemon: [pokemons[0]],
+    };
+  }
+
+  nextPokemon = () => {
+    if (totalPokes < pokemons.length) {
+      this.setState({
+        pokemon: [pokemons[totalPokes]],
+      });
+      totalPokes += 1
+    } else {
+      totalPokes = 1;
+      this.setState({
+        pokemon: [pokemons[0]],
+      });
+    }    
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <header> POKEDEX - leandromarten1 </header>
+        <div className="pokedex">
+          <Button click={this.nextPokemon} />
+          <Pokedex allPokdemons={this.state.pokemon} />
+        </div>
+      </div>
+    );
+  }
+}
+
+export default App;
