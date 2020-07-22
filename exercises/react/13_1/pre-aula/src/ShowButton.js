1a33173a6c35e468deb22e1160a1fb0d3aba7bd9
Leandro Marten <leandromarten1@gmail.com>
2020-07-22 16:06:47 -0300

    Remove all files Error

--- exercises/react/13_1/pre-aula/src/ShowButton.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-const ShowButton = (props) => {
-  const { changeTitle, value } = props;
-  return (
-    <button type="button" onClick={() => changeTitle(value)}>SHOW</button>
-  )
-}
-export default ShowButton;
--- exercises/react/practice/forms-redux/src/store/index.js
+++
@@ -1,8 +0,0 @@
-import { createStore, combineReducers } from 'redux';
-import listReducer from '../reducer/index';
-
-const root = combineReducers({ listReducer });
-
-const store = createStore(root);
-
-export default store;
