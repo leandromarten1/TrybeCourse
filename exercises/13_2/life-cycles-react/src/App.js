8869abac02d3efb2a406e3e620704ddf37999645
Leandro Marten <leandromarten1@gmail.com>
2020-07-24 03:17:09 -0300

    add Exercise 13.2

---
+++ exercises/13_2/life-cycles-react/src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './App.css';
+import RandomDog from './RandomDog';
+
+function App() {
+  return (
+    <div className="App">
+      <RandomDog />
+    </div>
+  );
+}
+
+export default App;
